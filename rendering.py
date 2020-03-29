7b6f287dc58ea9d48019d0cecd6df15fda7fa5df
Nikolay Chechulin <chechulin646@gmail.com>
2020-03-29 19:40:05 +0300

    make pylint like rendering.py

--- rendering.py
+++ rendering.py
@@ -1,3 +1,5 @@
+"""This module contains functions for converting LaTeX and Markdown files"""
+
 import string
 import random
 import os
@@ -6,33 +8,36 @@ import pdfkit
 
 
 def generate_random_name(size=120, alphabet=string.ascii_letters):
+    """This fuction generates a random name from file. The length and alphabet can be changed"""
     return ''.join(random.choice(alphabet) for _ in range(size))
 
 
 def render_markdown(code):
+    """Returns path to generated PDF or None"""
     filename = 'TEMP/' + generate_random_name() + '.pdf'
 
     try:
         html_text = markdown(code, output_format='html4')
         pdfkit.from_string(html_text, filename)
-    except:
+    except Exception:
         return None
 
     return filename
 
 
 def render_latex(code):
+    """Returns path to generated PDF or None"""
     filename = 'TEMP/' + generate_random_name()
-    tex_file = filename + '.tex'
-    pdf_file = filename + '.pdf'
+    tex_file_path = filename + '.tex'
+    pdf_file_path = filename + '.pdf'
 
-    with open(tex_file, 'w') as f:
-        f.write(code)
+    with open(tex_file_path, 'w') as tex_file:
+        tex_file.write(code)
 
     try:
-        command = f'pdflatex -output-directory=TEMP {tex_file} > /dev/null'
+        command = f'pdflatex -output-directory=TEMP {tex_file_path} > /dev/null'
         os.system(command)
-    except:
+    except Exception:
         return None
 
-    return pdf_file
+    return pdf_file_path
