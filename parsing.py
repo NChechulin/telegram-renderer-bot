bdc4f89bea88e200f238956ef237f250f1519245
Nikolay Chechulin <chechulin646@gmail.com>
2020-04-04 18:09:28 +0300

    add support for parsing github

--- parsing.py
+++ parsing.py
@@ -1,16 +1,39 @@
 """Implementation of different input methods"""
 
 import requests
+from bs4 import BeautifulSoup
+
+
+def parse_github(url):
+    try:
+        html = requests.get(url).text
+        soup = BeautifulSoup(html, 'html.parser')
+        raw_button = soup.find('a', {'id': 'raw-url'})
+
+        rel_link = raw_button['href'].replace('/raw', '')
+
+        raw_url = 'http://raw.githubusercontent.com' + rel_link
+
+        return parse_link(raw_url)
+    except Exception:
+        return None
+
 
 def parse_link(url: str):
     """Returns code from specified url. Only raw pages are supported right now."""
     if url.startswith('www.'):
         url = 'http://' + url[4:]
 
+    url = url.replace('https://', 'http://')
+
+    if url.startswith('http://github.com/'):
+        return parse_github(url)
+
     try:
         return requests.get(url).text
-    except:
-        raise Exception(f"Url {url} could not be opened")
+    except Exception:
+        return None
+
 
 def parse_text(user_input: str):
     """Returns code took from link or user input"""
